b141e59a36a311a707fac6d97bf8d17a0297d994
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-03 07:33:06 +0200

    social links

---
+++ config/socials.php
@@ -0,0 +1,38 @@
+<?php
+return [
+                [
+                    'name'=> 'Facebook',
+                    'link'=> '#',
+                    'icon'=> 'resources/footer-facebook.png',
+                    'target'=> '_blank'
+
+                ],
+                [
+                    'name'=> 'Twitter',
+                    'link'=> '#',
+                    'icon'=> 'resources/footer-twitter.png',
+                    'target'=> '_blank'
+
+                ],
+                [
+                    'name'=> 'You-tube',
+                    'link'=> '#',
+                    'icon'=> 'resources/footer-youtube.png',
+                    'target'=> '_blank'
+
+                ],
+                [
+                    'name'=> 'Pinterest',
+                    'link'=> '#',
+                    'icon'=> 'resources/footer-pinterest.png',
+                    'target'=> '_blank'
+
+               ],
+                [
+                    'name'=> 'Periscope',
+                    'link'=> '#',
+                    'icon'=> 'resources/footer-periscope.png',
+                    'target'=> '_blank'
+
+                ]
+                ];
