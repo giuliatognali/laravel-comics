198ccb7866fe4e4934afc78997c22cf9448657c5
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-03 10:09:20 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('page.title')</title>
     @vite('resources/js/app.js')
 </head>
 @include('partials.header')
-    <body>
-        @yield('page.main')
-    </body>
-    @include('partials.footer')
+
+<body>
+    @yield('page.main')
+</body>
+
+@include('partials.footer')
+
 </html>
