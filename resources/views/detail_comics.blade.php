eca684fc7078279faa7fbf11067678d1e8ee27b2
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-04 09:21:01 +0200

    det info comics

--- resources/views/detail_comics.blade.php
+++ resources/views/detail_comics.blade.php
@@ -16,29 +16,70 @@
         </div>
         <div class="container-small row">
             <div class="card-info col-8">
-                <h3>{{$comics['title']}}</h3>
+                <h3>{{ $comics['title'] }}</h3>
                 <div class="buy">
-                    <h6>Price {{$comics['price']}}</h6>
+                    <h6>Price {{ $comics['price'] }}</h6>
                 </div>
 
-                <p> {{$comics['description']}}
+                <p> {{ $comics['description'] }}
                 </p>
 
             </div>
             <div class="advs col-4">
                 <h6>ADVERTISEMENT</h6>
                 <a href="">
-                    <img class="img" src="{{Vite::asset('/resources/img/advisor.jpeg') }}" alt="">
+                    <img class="img" src="{{ Vite::asset('/resources/img/advisor.jpeg') }}" alt="">
                 </a>
             </div>
         </div>
         <div class="ms-container info">
-            <div class="container-small row">
-                <div class="col-6">
+            <div class="container-small row ">
+                     {{-- Talent --}}
+                <div class="col-6 ps-0 pe-4">
+               
                     <h5>Talent</h5>
+                    <div class="d-flex">
+                        <div class="col-4">
+                            <p>Art by:</p>
+                        </div>
+                        <p class="name-tag">
+                            @foreach ($comics['artists'] as $artist)
+                                {{ $artist }}
+                            @endforeach
+                        </p>
+                    </div>
+                    <div class="d-flex">
+                        <div class="col-4">
+                            <p>Written by:</p>
+                        </div>
+                        <p class="name-tag">
+                            @foreach ($comics['writers'] as $writer)
+                                {{ $writer }}
+                            @endforeach
+                        </p>
+                    </div>
                 </div>
-                <div class="col-6">
+                {{-- Specs --}}
+                <div class="col-6 pe-0 ps-4">
                     <h5>Specs</h5>
+                    <div class="d-flex">
+                        <div class="col-4">
+                            <p>Series:</p>
+                        </div>
+                        <p class="name-tag">{{$comics['series']}}</p>
+                    </div>
+                    <div class="d-flex">
+                        <div class="col-4">
+                            <p>U.S. Price:</p>
+                        </div>
+                        <p >{{$comics['price']}}</p>
+                    </div>                    <div class="d-flex">
+                        <div class="col-4">
+                            <p>On Sale Date:</p>
+                        </div>
+                        <p>{{$comics['sale_date']}}</p>
+                    </div>
+
                 </div>
 
             </div>
